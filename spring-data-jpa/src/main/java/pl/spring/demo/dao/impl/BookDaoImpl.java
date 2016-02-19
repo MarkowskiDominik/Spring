685d8c9c02319dfcd606cd2cb68163b149967248
Dominik <markowski.dominik.j@gmail.com>
2016-02-19 10:39:53 +0100

    solved exercise 1 - Transfer the beans configuration from XML to inject annotations

--- spring-data-jpa/src/main/java/pl/spring/demo/dao/impl/BookDaoImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/dao/impl/BookDaoImpl.java
@@ -10,10 +10,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("bookDao")
 public class BookDaoImpl implements BookDao {
 
     private final Set<BookTo> ALL_BOOKS = new HashSet<>();
 
+    @Autowired
     private Sequence sequence;
 
     public BookDaoImpl() {
@@ -51,9 +56,9 @@ public class BookDaoImpl implements BookDao {
 
     private void addTestBooks() {
         ALL_BOOKS.add(new BookTo(1L, "Romeo i Julia", "Wiliam Szekspir"));
-        ALL_BOOKS.add(new BookTo(2L, "Opium w rosole", "Hanna Ożogowska"));
+        ALL_BOOKS.add(new BookTo(2L, "Opium w rosole", "Hanna OÅ¼ogowska"));
         ALL_BOOKS.add(new BookTo(3L, "Przygody Odyseusza", "Jan Parandowski"));
-        ALL_BOOKS.add(new BookTo(4L, "Awantura w Niekłaju", "Edmund Niziurski"));
+        ALL_BOOKS.add(new BookTo(4L, "Awantura w NiekÅ‚aju", "Edmund Niziurski"));
         ALL_BOOKS.add(new BookTo(5L, "Pan Samochodzik i Fantomas", "Zbigniew Nienacki"));
         ALL_BOOKS.add(new BookTo(6L, "Zemsta", "Aleksander Fredro"));
     }
